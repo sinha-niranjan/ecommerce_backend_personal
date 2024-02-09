21d4a5976a766755f134522ca0d9d5bd9e812673
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-02-09 11:09:51 +0530

    utils configured

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import express from "express";
 import { config } from "dotenv";
+import { app } from "./app.js";
 import { connectDB } from "./db/index.js";
 
 config({ path: "./.env" });
 const port = process.env.PORT || 4000;
 const mongoURI = process.env.MONGO_URI || "";
 
-const app = express();
-
-connectDB(mongoURI);
-
-app.listen(port, () => {
-  console.log(`server is working on Port : ${port}`);
-});
+connectDB(mongoURI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running  on Port : ${port}`);
+    });
+  })
+  .catch((error) => console.log("mongodb connection failed ", error));
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,10 @@
+export const asyncHandler = (fun) => async (req, res, next) => {
+  try {
+    await fun(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
